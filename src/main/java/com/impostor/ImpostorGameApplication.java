0cad155e5e2cfb7c3ebcebefae4a3ab710880d00
educole3335 <usr3335@salesianos-lacuesta.net>
2026-04-25 15:36:32 +0100

    KARMA config: move Spring Boot application entrypoint and refresh build outputs

--- src/main/java/com/impostor/ImpostorGameApplication.java
+++ src/main/java/com/impostor/ImpostorGameApplication.java
@@ -1,9 +1,11 @@
-package com.impostor.server;
+package com.impostor;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.impostor.server.GameServer;
 
 @SpringBootApplication(scanBasePackages = "com.impostor")
 public class ImpostorGameApplication {
